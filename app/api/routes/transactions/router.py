9399708f64ae5c0e97edd6c5d119589791ed0462
ina-ene <ina.ene.pancake@gmail.com>
2024-05-26 11:50:00 +0300

    added some things

--- app/api/routes/transactions/router.py
+++ app/api/routes/transactions/router.py
@@ -1,11 +1,23 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
-from core.db_dependency import get_db
+from app.core.db_dependency import get_db
 from .schemas import TransactionDTO
 from sqlalchemy.orm import Session
-from .service import create_transaction as ct
-from ..users.schemas import UserViewDTO
+from .service import create_draft_transaction as cdt
+from ..users.schemas import UserDTO, UserViewDTO
 from ...auth_service import auth
 
 
 transaction_router = APIRouter(prefix="/transactions", tags=["Transactions"])
+
+
+@transaction_router.post("/draft")
+def create_draft_transaction(
+    current_user: Annotated[UserViewDTO, Depends(auth.get_user_or_raise_401)],
+    transaction: TransactionDTO,
+    db: Session = Depends(get_db),
+):
+
+    created_draft_transaction = cdt(current_user.username, transaction, db)
+
+    return f"You are about to send {created_draft_transaction.amount} to {created_draft_transaction.receiver_account}"
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.core.database import engine
 from app.core import models
 from app.api.routes.categories.router import category_router
 from app.api.routes.users.router import user_router
+from app.api.routes.transactions.router import transaction_router
 import uvicorn
 
 models.Base.metadata.create_all(bind=engine)
@@ -11,6 +12,7 @@ app = FastAPI()
 
 app.include_router(category_router)
 app.include_router(user_router)
+app.include_router(transaction_router)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000)
