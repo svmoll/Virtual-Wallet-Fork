5aef78c08196a8381f59ebf57a8145026c196df7
svmoll <156302253+svmoll@users.noreply.github.com>
2024-05-28 12:56:12 +0300

    Create Card updates and some fixes

--- tests/card_tests/card_router_test.py
+++ tests/card_tests/card_router_test.py
@@ -1,9 +1,9 @@
 import asyncio
 import unittest
 from sqlalchemy.orm import sessionmaker
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from unittest.mock import patch, Mock, MagicMock, create_autospec
-from app.api.routes.users.schemas import UserDTO, UpdateUserDTO, UserViewDTO
+from app.api.routes.users.schemas import UserDTO, UpdateUserDTO, UserDTO
 from app.core.models import Card
 from app.api.routes.cards.router import create_card, delete_card
 from app.api.routes.cards.schemas import CardDTO, UserViewDTO
@@ -18,14 +18,6 @@ def fake_card():
         cvv= "123"
     )
 
-def fake_card_dto():
-    return CardDTO(
-        account_id=1,
-        card_number= "1111222233334444",
-        expiration_date= "2024-02-02",
-        card_holder= "Dimitar Berbatov",
-        cvv= "123"
-    )
 
 
 Session = sessionmaker()
@@ -36,29 +28,24 @@ def fake_db():
 
 
 def fake_user_view():
-    return UserViewDTO(id=1, username="testuser")
+    return UserDTO(id=1, username="testuser", fullname="Georgi Hristov")
 
 
 class CardRouter_Should(unittest.TestCase):
 
     @patch('app.api.routes.cards.router.create_card')
-    def test_createCard_IsSuccessful(self, create_mock):
-        #arrange
-        card = fake_card()
-        create_mock.return_value = card
+    def test_createCard_returnsCorrectStatusCodeWhenSuccessful(self, create_mock):
+        #Arrange
+        # card = fake_card()
+        # create_mock.return_value = card
 
         user = fake_user_view()
         db = fake_db()
-        # Act
-        async def async_test():
-            result = await create_card(user, db)
+        #Act
+        result = create_card(user, db)
         #Assert
-            create_mock.assert_called_once()
-            expected_result = card
-            self.assertEqual(expected_result, result)
-
-        asyncio.run(async_test())
-
-
+        create_mock.assert_called_once()
+        expected_result = status.HTTP_201_CREATED
+        self.assertEqual(expected_result, result)
 
 
