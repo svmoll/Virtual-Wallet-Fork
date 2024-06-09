c379728752f0d6c7e35d26753d5748bf6a9d6b77
ina-ene <ina.ene.pancake@gmail.com>
2024-06-09 23:54:42 +0300

    adding schemas + router

--- app/api/routes/transactions/schemas.py
+++ app/api/routes/transactions/schemas.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional, Literal
+from datetime import date, datetime
 
 
 class TransactionDTO(BaseModel):
@@ -7,3 +9,23 @@ class TransactionDTO(BaseModel):
     amount: Decimal
     category_id: int | None = None
     description: str | None = None
+
+
+class RecurringTransactionDTO(TransactionDTO):
+    recurring_interval: Literal[
+        "daily", "weekly", "monthly", "yearly", "minute", "custom"
+    ]
+    custom_days: Optional[int] = Field(None, description="Custom interval in days")
+    start_date: Optional[date] = Field(
+        None, description="Start date for the recurring transaction"
+    )
+
+
+class RecurringTransactionView(BaseModel):
+    id: int
+    receiver: str
+    amount: Decimal
+    category_id: Optional[int] = None
+    description: Optional[str]
+    recurring_interval: str
+    next_run_time: date
